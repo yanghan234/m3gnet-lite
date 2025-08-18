69017cf9969c3d27b9320aa92796dbc608324a93
Han Yang <yanghan234@outlook.com>
2025-08-18 12:38:40 +0800

    update graph converter and add property labels in the data object

--- src/m3gnet/graph/converter.py
+++ src/m3gnet/graph/converter.py
@@ -1,4 +1,5 @@
 import numpy as np
+import torch
 from ase.atoms import Atoms
 from pymatgen.core.structure import Structure
 from torch_geometric.data import Data
@@ -32,6 +33,10 @@ class GraphConverter:
         *,
         pos: np.ndarray,
         cell: np.ndarray | None = None,
+        atomic_numbers: np.ndarray | None = None,
+        potential_energy: float | None = None,
+        forces: np.ndarray | None = None,
+        stress: np.ndarray | None = None,
     ) -> Data:
         """
         Convert a set of atomic coordinates and cell parameters into a PyG graph.
@@ -64,26 +69,49 @@ class GraphConverter:
             three_body_indices = np.empty((0, 2), dtype=np.int64)
 
         return Data(
-            pos=pos,
-            edge_index=edge_index,
-            edge_attr=edge_dist,
-            three_body_indices=three_body_indices,
-            num_bonds_per_atom=num_bonds_per_atom,
-            num_angles_per_atom=num_angles_per_atom,
-            num_angles_on_central_atom=num_angles_on_central_atom,
-            total_num_angles=total_num_angles,
+            atomic_numbers=torch.tensor(atomic_numbers, dtype=torch.long),
+            pos=torch.tensor(pos, dtype=torch.float32),
+            edge_index=torch.tensor(edge_index, dtype=torch.long),
+            edge_dist=torch.tensor(edge_dist, dtype=torch.float32),
+            three_body_indices=torch.tensor(three_body_indices, dtype=torch.long),
+            num_bonds_per_atom=torch.tensor(num_bonds_per_atom, dtype=torch.long),
+            num_angles_per_atom=torch.tensor(num_angles_per_atom, dtype=torch.long),
+            num_angles_on_central_atom=torch.tensor(
+                num_angles_on_central_atom, dtype=torch.long
+            ),
+            total_num_angles=torch.tensor(total_num_angles, dtype=torch.long),
+            potential_energy=torch.tensor(potential_energy, dtype=torch.float32)
+            if potential_energy is not None
+            else None,
+            forces=torch.tensor(forces, dtype=torch.float32)
+            if forces is not None
+            else None,
+            stress=torch.tensor(stress, dtype=torch.float32)
+            if stress is not None
+            else None,
         )
 
     def convert_ase_atoms(self, atoms: Atoms, **kwargs) -> Data:
         """
         Convert an ASE Atoms object into a PyG graph.
         """
-        return self.convert(pos=atoms.positions, cell=atoms.cell, **kwargs)
+        return self.convert(
+            pos=atoms.positions,
+            cell=atoms.cell,
+            atomic_numbers=atoms.get_atomic_numbers(),
+            potential_energy=atoms.get_potential_energy(),
+            forces=atoms.get_forces(),
+            stress=atoms.get_stress(),
+            **kwargs,
+        )
 
     def convert_pymatgen_structure(self, structure: Structure, **kwargs) -> Data:
         """
         Convert a PyMatGen Structure object into a PyG graph.
         """
         return self.convert(
-            pos=structure.cart_coords, cell=structure.lattice.matrix, **kwargs
+            pos=structure.cart_coords,
+            cell=structure.lattice.matrix,
+            atomic_numbers=structure.atomic_numbers,
+            **kwargs,
         )
